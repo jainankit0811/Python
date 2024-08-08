a1d3b73450986ab6b374c7b3f882ab0aefca1ed7
jainankit08 <ankitd0811@gmail.com>
2024-08-08 21:32:58 +0530

    update

--- .venv/bin/activate.fish
+++ .venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/ankit/Data-Science/.venv"
+set -gx VIRTUAL_ENV "/home/ankit/Data-Science/Python/.venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
